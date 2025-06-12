3b9ad643faca78803a270affe07ddefe13900bc3
Alex <alexandercastronovo@gmail.com>
2025-06-11 22:37:18 -0400

    vertical velocity not working :(

--- analysis.py
+++ analysis.py
@@ -1,6 +1,7 @@
+import numpy as np
 
 
-def smooth_outliers(df, col):
+def smooth_outliers(df, col, threshold_multiplier=2, derivative_smoothing=True):
     vals = df[col].dropna()
     mean_val = vals.mean()
     mad = (vals - mean_val).abs().mean()  # Mean absolute deviation
@@ -8,5 +9,8 @@ def smooth_outliers(df, col):
     rolling_median = df[col].rolling(window=5, center=True).median()
     diff = (df[col] - rolling_median).abs()  # Absolute deviation from rolling mean
 
-    return df[diff < threshold]
+    # if the derivative is greater than a threshold, consider it an outlier
+    derivative = df[col].diff().abs()
+
+    return df[(diff < threshold) & (derivative < (50 if derivative_smoothing else np.inf))].copy()
 
